b760cc1ad6b91da456b448e01cc4b2e8434de0f6
Salman-Faiz <salmanfaiz.cse@gmail.com>
2024-05-13 02:09:16 +0600

    Updating State In Arrays And Pass Parameter

--- src/Components/Countries/Countries.jsx
+++ src/Components/Countries/Countries.jsx
@@ -12,25 +12,49 @@ const Countries = () => {
 
 
     const [countries, setCountries] = useState([]);
+    const [visitedCountries,setVisitedCountries] = useState([]);
     useEffect(() => {
         fetch('https://restcountries.com/v3.1/all')
-        .then(res => res.json())
-        // .then(data=> console.log(data))
-        .then(data=> setCountries(data))
+            .then(res => res.json())
+            // .then(data=> console.log(data))
+            .then(data => setCountries(data))
 
 
     }, [])
+
+    const handleVisitedCountries = country =>{
+            
+        console.log('add this to your visited countries');
+       const newVisitedCountry = [...visitedCountries,country]
+
+       setVisitedCountries(newVisitedCountry);
+    }
 
 
     return (
         <div>
             <h4>Total Countries : {countries.length}</h4>
-            <div className="countriesDiv">
-            {
-                countries.map(country => <Country key={country.ccn3} country={country}></Country>)
-            }    
+            <div>
+                <h4>Visited Countries: {visitedCountries.length}</h4>
+                <ol>
+                    {
+                        visitedCountries.map(country => <li>{country.name.common}
+                        </li>)
+                    }
+                </ol>
             </div>
-            
+            <div className="countriesDiv">
+                {
+                    countries.map(country => <Country
+                        key={country.ccn3}
+                        handleVisitedCountries={handleVisitedCountries}
+                        country={country}
+                        
+                        >
+                    </Country>)
+                }
+            </div>
+
         </div>
     );
 };
--- src/Components/Country/Country.jsx
+++ src/Components/Country/Country.jsx
@@ -1,19 +1,27 @@
 import { useState } from 'react';
 import './Country.css'
-const Country = ({ country }) => {
+const Country = ({ country ,handleVisitedCountries }) => {
     const { name, flags, population, cca3 } = country;
     const [visited, setVisited] = useState(false);
 
     const handleVisited = () =>{
         setVisited(!visited);
+     
+     
         // bank sign use for toggle the button.. !true = false;
+
+
     }
+
+   
     return (
-        <div className="countryDiv">
-            <h3>{name.common}</h3>
+        <div className={`countryDiv ${visited && 'visited'}`}>
+            <h3 style={{color: visited ? 'yellow' : 'black'}}>{name.common}</h3>
             <img src={flags.png} alt="" />
             <p><small>Population: {population}</small></p>
             <p><small>COde: {cca3}</small></p>
+            <button onClick={()=>{handleVisitedCountries(country)}}>Mark Visited</button>
+            <br /><br />
             <button onClick={handleVisited}>{visited ? 'visited' : 'Going'}</button>
             {
                 // visited && 'visited this country.'
